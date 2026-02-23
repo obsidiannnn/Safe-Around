64d908c2e6eddf9d11ef304e2dce705f48efc845
obsidiannnn <satya.chauhan1976@gmail.com>
2026-02-23 21:59:35 +0530

    HTTP server

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("SafeAround")
+	mux := http.NewServeMux()
+
+	// Health check
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	log.Println("SafeAround API running on :8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
